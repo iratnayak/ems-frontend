8c7de491bab2153f4b2d46733a43c5d316b9d715
iratnayak <iratnayake6@gmail.com>
2025-11-21 00:01:36 +0530

    feat: Update Employees details

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react";
 function App() {
   const [employees, setEmployees] = useState([]);
 
-  // Form Fields
+// Form Fields
   const [name, setName] = useState("");
   const [basic, setBasic] = useState("");
   const [otHours, setOtHours] = useState("");
   const [otRate, setOtRate] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
 
 // Load employees from API
   const loadData = () => {
@@ -20,7 +22,7 @@ function App() {
     loadData();
 
   }, []);
-
+// Add Employee
   const addEmployee = () => {
     if (!name || !basic || !otHours || !otRate){
       alert("Please Fill all fields");
@@ -56,6 +58,66 @@ function App() {
     })
     .catch((err) => console.error("Add Error! ", err));
   }
+// Edit Employee Details
+  const startEdit = (emp, index) => {
+    setName(emp.name);
+    setBasic(emp.basic);
+    setOtHours(emp.otHours);
+    setOtRate(emp.otRate);
+    setIsEdit(true);
+    setEditIndex(index);
+  };
+
+  const updateEmployee = () => {
+    const basicNum = parseFloat(basic);
+    const otHoursNum = parseFloat(otHours);
+    const otRateNum = parseFloat(otRate);
+
+    const otAmount = otHoursNum * otRateNum;
+    const fullSalary = otAmount + basic;
+
+    const updatedEmp = {
+      name,
+      basic: basicNum,
+      otHours: otHoursNum,
+      otRate: otRateNum,
+      otAmount,
+      fullSalary,
+    };
+
+    fetch(`http://localhost:5001/employees/${editIndex}`, {
+      method: "PUT",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(updatedEmp),
+    })
+    .then((res) => res.json())
+    .then(() => {
+      loadData();
+      cancelEdit();
+    })
+    .catch((err) => console.error("Update Error!", err));
+  };
+
+// Cancel Edit Employee Details
+  const cancelEdit = () => {
+    setIsEdit(false);
+    setEditIndex(null);
+    setName("");
+    setBasic("");
+    setOtHours("");
+    setOtRate("");
+  };
+
+  // Delete Employee
+  const deleteEmployee = (index) => {
+    fetch(`http://localhost:5001/employees/${index}`,{
+      method: "DELETE", 
+    })
+    .then((res) => res.json())
+    .then(() => loadData())
+    .catch((err) => console.error("Delete Error:", err));
+
+  };
 
   return (
     <div
@@ -97,7 +159,14 @@ function App() {
           onChange={(e) => setOtRate(e.target.value)}
           style={{ marginRight: "8px" }}
         />
-        <button onClick={addEmployee}>Add Employee</button>
+        {isEdit ? (
+          <>
+            <button onClick={updateEmployee}>Update</button>
+            <button onClick={cancelEdit} style={{ marginLeft: "8px" }}>Cancel</button>
+          </>
+             ) : (
+            <button onClick={addEmployee}>Add Employee</button>
+             )}
       </div>
 
       {/* Employee Table */}
@@ -108,6 +177,7 @@ function App() {
             <th>Basic</th>
             <th>OT</th>
             <th>Full Salary</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
@@ -117,6 +187,12 @@ function App() {
               <td>{e.basic}</td>
               <td>{e.otAmount}</td>
               <td>{e.fullSalary}</td>
+              <td>
+                <button onClick={() => startEdit(e, index)}>Edit</button>
+                <button onClick={() => deleteEmployee(index)} style={{ marginLeft: "8px" }}>
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
